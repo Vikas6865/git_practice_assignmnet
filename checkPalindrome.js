a981d6249921f4d40fbfada7116319474ca7d828
vishal <vt386282@gmail.com>
2022-12-06 13:57:01 +0530

    code fix

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,8 +1,8 @@
-let str="vikas"
-let bag=''
-for(i=str.length-1;i>=0;i--){
-  bag=str[i]
-  }
-if(bag==str){
-console.log("string is palindrome")
+let str = "vikas"
+let bag = ''
+for (i = str.length - 1; i >= 0; i--) {
+  bag+= str[i]
+}
+if (bag == str) {
+  console.log("string is palindrome")
 }
